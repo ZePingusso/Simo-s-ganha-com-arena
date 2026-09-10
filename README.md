4c01d71a4fb7ef8fc78129892265c2fb1becb2b3
ZePingusso <777popopipi@gmail.com>
2026-09-10 16:02:28 -0400

    adicionando os trem da aula do renato

--- README.md
+++
@@ -1,4 +0,0 @@
-# Simo-s-ganha-com-arena
-Aula de PTAS na info 4A - IFMS
-Testando alteração
-Como andar de carro com vózinhas
--- server.js
+++
@@ -1,9 +0,0 @@
-import http from "http"
-
-const server  = http.createServer((req, res) => {
-    console.log(req.method)
-    console.log(req.url)
-    res.end('<h1> Simo Hayha</h1><p>Loki podre</p>')
-})
-
-server.listen(2222)
